d8918bb0a998d96e4fc9e70ecd2126f7a2fd6b55
Pulipuli Chen <pulipuli.chen@gmail.com>
2014-02-18 01:33:19 +0800

    0218 建立compress_debug_log.sh

---
+++ wp-content/compress_debug.log.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BASEDIR=$(dirname $0)
+
+DEBUG_LOG=$BASEDIR/debug.log
+
+SIZE=`stat -c %s $DEBUG_LOG`
+
+if [ $SIZE -gt 104857600 ]; then
+    # 如果大小超過100MB
+    zip -rqm9 $BASEDIR/archive_debug_log/debug.log-$(date +%F).zip $BASEDIR/debug.log
+    touch $BASEDIR/debug.log
+fi
